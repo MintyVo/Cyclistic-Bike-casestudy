0a3159a900d14fc19f6a350ac9d57537cd144d70
Minty Vo <98665643+MintyVo@users.noreply.github.com>
2022-02-05 10:12:47 -0800

    import all data into .r and check the structure of the data

--- Cyclistic_Bike_Casestudy.R
+++ Cyclistic_Bike_Casestudy.R
@@ -15,3 +15,60 @@ library(here)
 library(skimr)
 library(janitor)
 
+#import data 
+setwd("/Users/minty/Developer/Cyclistic-Bike-casestudy")
+feb2021 <- read_csv("202102-divvy-tripdata.csv")
+str(feb2021)
+head(feb2021)
+
+mar2021 <- read_csv("202103-divvy-tripdata.csv")
+str(mar2021)
+head(mar2021)
+
+apr2021 <- read_csv("202104-divvy-tripdata.csv")
+str(apr2021)
+head(apr2021)
+
+ma2021 <- read_csv("202105-divvy-tripdata.csv")
+str(ma2021)
+head(ma2021)
+
+jun2021 <- read_csv("202106-divvy-tripdata.csv")
+str(jun2021)
+head(jun2021)
+
+jul2021 <- read_csv("202107-divvy-tripdata.csv")
+str(jul2021)
+head(jul2021)
+
+aug2021 <- read_csv("202108-divvy-tripdata.csv")
+str(aug2021)
+head(aug2021)
+
+sep2021 <- read_csv("202109-divvy-tripdata.csv")
+str(sep2021)
+head(sep2021)
+
+oct2021 <- read_csv("202110-divvy-tripdata.csv")
+str(oct2021)
+head(oct2021)
+
+nov2021 <- read_csv("202111-divvy-tripdata.csv")
+str(nov2021)
+head(nov2021)
+
+dec2021 <- read_csv("202112-divvy-tripdata.csv")
+str(dec2021)
+head(dec2021)
+
+jan20212 <- read_csv("202201-divvy-tripdata.csv")
+str(jan2022)
+head(jan2022)
+
+
+
+
+
+
+
+
